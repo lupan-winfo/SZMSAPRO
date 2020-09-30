fa7f3e20bb075a80aa97ae8c9592e1f1ca0f3130
lupan <422820935@qq.com>
2020-09-30 13:28:48 +0800

    运行控制入口文件修改完善

--- run/runner.py
+++ run/runner.py
@@ -12,7 +12,7 @@ class RunCase(unittest.TestCase):
         f = open(cd.creatName(), 'wb')
         # 获取当前文件的路径。其中os.getcwd()是获取当前工作目录
         case_path = os.path.abspath('..') + "/case/"
-        suite = unittest.defaultTestLoader.discover(case_path,'sz*.py')
+        suite = unittest.defaultTestLoader.discover(case_path,'loginCase*.py')
         runner = HTMLTestRunner.HTMLTestRunner(stream=f, title="this is SZPro UI automation report", description=u"基础产品PC端登录服务测试报告",
                                                verbosity=2)
         runner.run(suite)
